76b961fb46957c92bd6ee4c734c780ccc810c17b
derikon <derikon@yandex-team.ru>
2013-04-22 21:05:30 +0400

    Tests can use both local and remote elliptics
    Removed unused code

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,9 +4,12 @@
 #include <cstdlib>
 #include <sstream>
 #include <cstring>
+#include <algorithm>
+#include <iterator>
 
 #include <elliptics/proxy.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/tokenizer.hpp>
 
 #include <cppunit/TestResult.h>
 #include <cppunit/extensions/HelperMacros.h>
@@ -15,20 +18,97 @@
 
 using namespace elliptics;
 
-class elliptics_tests_t : public CppUnit::TestFixture {
+class elliptics_manager_t {
 public:
-	elliptics_tests_t(const std::string &script)
+	virtual elliptics_proxy_t::config config() = 0;
+	virtual ~elliptics_manager_t() {}
+};
+
+class script_manager_t : public elliptics_manager_t {
+public:
+	script_manager_t(const std::string &script)
 		: m_script(script)
 	{
 		std::system((script + " prepare 4").c_str());
 		nodes<start>();
+	}
 
+	~script_manager_t() {
+		std::system((m_script + " clear").c_str());
+	}
+
+	elliptics_proxy_t::config config() {
 		elliptics_proxy_t::config elconf;
 		elconf.groups.push_back(1);
 		elconf.groups.push_back(2);
-		elconf.log_mask = 1;
 		//elconf.cocaine_config = std::string("/home/derikon/cocaine/cocaine_config.json");
 		elconf.remotes.push_back(elliptics_proxy_t::remote("localhost", 1025, 2));
+		return elconf;
+	}
+
+private:
+	
+	enum node_action { start, stop };
+
+	template<node_action na>
+	void node(size_t num) {
+		std::ostringstream oss;
+		oss << m_script;
+		if(na == start) oss << " start ";
+		else if (na == stop) oss << " stop ";
+		oss << num;
+		std::system(oss.str().c_str());
+	}
+
+	template<node_action na>
+	void nodes(std::initializer_list<size_t> nums) {
+		for(auto it = nums.begin(); it != nums.end(); ++it)
+			node<na>(*it);
+	}
+
+	template<node_action na>
+	void nodes() {
+		std::ostringstream oss;
+		oss << m_script;
+		if(na == start) oss << " start ";
+		else if (na == stop) oss << " stop ";
+		std::system(oss.str().c_str());
+	}
+
+	std::string m_script;
+};
+
+class simple_manager_t : public elliptics_manager_t {
+public:
+	simple_manager_t(const std::string &hostname, int port, int family, 
+						const std::vector<int> &groups) 
+		: m_hostname(hostname)
+		, m_port(port)
+		, m_family(family)
+	{
+	}
+
+	elliptics_proxy_t::config config() {
+		elliptics_proxy_t::config elconf;
+		std::copy(m_groups.begin(), m_groups.end(), std::back_inserter(elconf.groups));
+		//elconf.cocaine_config = std::string("/home/derikon/cocaine/cocaine_config.json");
+		elconf.remotes.push_back(elliptics_proxy_t::remote(m_hostname, m_port, m_family));
+		return elconf;
+	}
+private:
+	std::string m_hostname;
+	int m_port;
+	int m_family;
+	std::vector<int> m_groups;
+};
+
+class elliptics_tests_t : public CppUnit::TestFixture {
+public:
+	elliptics_tests_t(const std::shared_ptr<elliptics_manager_t> &elliptics_manager)
+		: m_elliptics_manager(elliptics_manager)
+	{
+		elliptics_proxy_t::config elconf = m_elliptics_manager->config();
+		elconf.log_mask = 1;
 		elconf.success_copies_num = SUCCESS_COPIES_TYPE__ALL;
 		elconf.chunk_size = 16;
 
@@ -38,7 +118,6 @@ public:
 
 	~elliptics_tests_t() {
 		m_proxy.reset();
-		std::system((m_script + " clear").c_str());
 	}
 
 	void remove() {
@@ -225,338 +304,87 @@ private:
         return m_proxy->write(elliptics::key_t(key), std::string("test data"), _offset = offset, _size = size, _cflags = cflags, _ioflags = ioflags, _groups = groups, _success_copies_num = success_copies_num);
     }
 
-	enum node_action { start, stop };
-
-	template<node_action na>
-	void node(size_t num) {
-		std::ostringstream oss;
-		oss << m_script;
-		if(na == start) oss << " start ";
-		else if (na == stop) oss << " stop ";
-		oss << num;
-		std::system(oss.str().c_str());
-	}
-
-	template<node_action na>
-	void nodes(std::initializer_list<size_t> nums) {
-		for(auto it = nums.begin(); it != nums.end(); ++it)
-			node<na>(*it);
-	}
-
-	template<node_action na>
-	void nodes() {
-		std::ostringstream oss;
-		oss << m_script;
-		if(na == start) oss << " start ";
-		else if (na == stop) oss << " stop ";
-		std::system(oss.str().c_str());
-	}
-
-	std::string m_script;
+	std::shared_ptr<elliptics_manager_t> m_elliptics_manager;
 	std::shared_ptr<elliptics_proxy_t> m_proxy;
-};
-
-void test_lookup (elliptics_proxy_t &proxy) {
-	std::cout << __func__ << std::endl;
-
-	elliptics::key_t k(std::string("key.txt"));
-
-	try { proxy.remove (k); } catch (...) {}
-
-	std::string data("data");
-
-	std::vector <int> g = {2};
-	std::vector<lookup_result_t> l = proxy.write(k, data/*, _groups = g*/);
-	std::cout << "written " << l.size() << " copies" << std::endl;
-	for (auto it = l.begin(); it != l.end(); ++it) {
-		std::cout << "\tpath: " << it->hostname << ":" << it->port << it->path << std::endl;
-	}
-
-	auto l1 = proxy.lookup(k);
-	std::cout << "lookup path: " << l1.hostname << ":" << l1.port << l1.path << std::endl;
-}
-
-void test_async (elliptics_proxy_t &proxy) {
-	std::cout << __func__ << std::endl;
-
-	elliptics::key_t k1(std::string("key1.txt"));
-	elliptics::key_t k2(std::string("key2.txt"));
-
-	try { proxy.remove_async (k1).get (); } catch (...) {}
-	try { proxy.remove_async (k2).get (); } catch (...) {}
-
-	std::string data1("data1");
-	std::string data2("data2");
-
-	std::vector<ioremap::elliptics::lookup_result_entry> l;
-
-	auto awr1 = proxy.write_async(k1, data1);
-	auto awr2 = proxy.write_async(k2, data2);
-
-	try {
-		l = awr1.get ();
-	} catch (...) {
-		std::cout << "Exception during get write result" << std::endl;
-		return;
-	}
-	std::cout << "written " << l.size() << " copies" << std::endl;
-	for (auto it = l.begin(); it != l.end(); ++it) {
-		//std::cout << "\tpath: " << it->hostname << ":" << it->port << it->path << std::endl;
-		auto host = proxy.get_host (*it);
-		auto path = proxy.get_path (*it);
-		std::cout << "\tpath: " << host.host << ":" << host.port << path << std::endl;
-	}
-
-	try {
-		l = awr2.get ();
-	} catch (...) {
-		std::cout << "Exception during get write2 result" << std::endl;
-		return;
-	}
-	std::cout << "written " << l.size() << " copies" << std::endl;
-	for (auto it = l.begin(); it != l.end(); ++it) {
-		//std::cout << "\tpath: " << it->hostname << ":" << it->port << it->path << std::endl;
-		auto host = proxy.get_host (*it);
-		auto path = proxy.get_path (*it);
-		std::cout << "\tpath: " << host.host << ":" << host.port << path << std::endl;
-	}
-
-	auto arr1 = proxy.read_async(k1);
-	auto arr2 = proxy.read_async(k2);
-
-	data_container_t ds;
-	try {
-		ds = arr1.get();
-	} catch (...) {
-		std::cout << "Exception during get read result" << std::endl;
-		return;
-	}
-	std::cout << "Read result: " << ds.data.to_string() << std::endl;
-
-	try {
-		ds = arr2.get();
-	} catch (...) {
-		std::cout << "Exception during get read result" << std::endl;
-		return;
-	}
-	std::cout << "Read result: " << ds.data.to_string() << std::endl;
-}
-
-void test_sync (elliptics_proxy_t &proxy) {
-	std::cout << __func__ << std::endl;
-
-	elliptics::key_t k1(std::string("key1.txt"));
-
-	try { proxy.remove (k1); } catch (...) {}
-
-	std::string data1("data1");
-
-	std::vector<lookup_result_t> l = proxy.write(k1, data1);
-
-	std::cout << "written " << l.size() << " copies" << std::endl;
-	for (auto it = l.begin(); it != l.end(); ++it) {
-		std::cout << "\tpath: " << it->hostname << ":" << it->port << it->path << std::endl;
-	}
-
-	std::cout << "Read result: " << proxy.read(k1).data.to_string() << std::endl;
-}
-
-void test_sync_embeds (elliptics_proxy_t &proxy) {
-	std::cout << __func__ << std::endl;
-
-	elliptics::key_t k1(std::string("key1.txt"));
-
-	try { proxy.remove (k1); } catch (...) {}
-
-	data_container_t ds("data1");
-	timespec ts;
-	ts.tv_sec = 123;
-	ts.tv_nsec = 456000;
-	ds.set<elliptics::DNET_FCGI_EMBED_TIMESTAMP>(ts);
-
-	std::vector<lookup_result_t> l = proxy.write(k1, ds);
-
-	std::cout << "written " << l.size() << " copies" << std::endl;
-	for (auto it = l.begin(); it != l.end(); ++it) {
-		std::cout << "\tpath: " << it->hostname << ":" << it->port << it->path << std::endl;
-	}
-
-	auto rr = proxy.read(k1, _embeded = true);
-	std::cout << "Read result: " << rr.data.to_string() << std::endl;
-	auto r0 = rr.get<elliptics::DNET_FCGI_EMBED_TIMESTAMP>();
-	if (r0) {
-		timespec &ts = *r0;
-		std::cout << "Embed result: " << ts.tv_sec << ' ' << ts.tv_nsec << std::endl;
-	}
-	else
-		std::cout << "Embed result: none" << std::endl;
-}
-
-void test_bulk_sync(elliptics_proxy_t &proxy) {
-	std::cout << __func__ << std::endl;
-
-	std::vector <elliptics::key_t> keys = {std::string ("key5"), std::string ("key6")};
-	std::vector <data_container_t> data_arr;
-	data_arr.emplace_back("data1");
-	data_arr.emplace_back("data2");
-	std::vector <int> groups = {1, 2};
-
-	try { proxy.remove (keys[0]); } catch (...) {}
-	try { proxy.remove (keys[1]); } catch (...) {}
-
-	{
-		auto results = proxy.bulk_write (keys, data_arr, _groups = groups);
-		for (auto it = results.begin (), end = results.end (); it != end; ++it) {
-			std::cout << it->first.to_string () << ':' << std::endl;
-			for (auto it2 = it->second.begin (), end2 = it->second.end (); it2 != end2; ++it2) {
-				std::cout << "\tgroup: " << it2->group << "\tpath: " << it2->hostname
-									  << ":" << it2->port << it2->path << std::endl;
-			}
-		}
-	}
-
-	{
-		auto result = proxy.bulk_read (keys, _groups = groups);
-		for (auto it = result.begin (), end = result.end (); it != end; ++it) {
-			std::cout << it->second.data.to_string() << std::endl;
-		}
-	}
-}
-
-void test_mastermind_groups(elliptics_proxy_t &proxy) {
-	std::cout << __func__ << std::endl;
-
-	auto r1 = proxy.get_symmetric_groups();
-	std::cout << "get_symmetric_groups: " << std::endl;
-	std::cout << "size = " << r1.size() << std::endl;
-	for (size_t i = 0; i != r1.size(); ++i) {
-		std::cout << "\tsize = " << r1[i].size() << std::endl;
-		std::cout << "\t\t";
-		for (size_t j = 0; j != r1[i].size(); ++j) {
-			std::cout << r1[i][j] << ' ';
-		}
-		std::cout << std::endl;
-	}
-
-	auto r2 = proxy.get_bad_groups();
-	std::cout << "get_bad_groups: " << std::endl;
-	for (auto it = r2.begin(); it != r2.end(); ++it) {
-		std::cout << it->first << std::endl;
-		std::cout << "\tsize: " << it->second.size() << std::endl;
-		std::cout << "\t\t";
-		for (auto jt = it->second.begin(); jt != it->second.end(); ++jt) {
-			std::cout << *jt << ' ';
-		}
-		std::cout << std::endl;
-	}
-
-	auto r3 = proxy.get_all_groups();
-	std::cout << "get_all_groups: " << std::endl;
-	for (auto it = r3.begin(); it != r3.end(); ++it) {
-		std::cout << *it << ' ';
-	}
-	std::cout << std::endl;
-}
-
-class tester {
-public:
-	typedef std::function<void (elliptics_proxy_t &)> test_t;
-	typedef std::initializer_list<test_t> tests_t;
-
-	tester(const std::string &host, int port, int family) {
-		elliptics_proxy_t::config elconf;
-		elconf.groups.push_back(1);
-		elconf.groups.push_back(2);
-		elconf.log_mask = 1;
-		elconf.cocaine_config = std::string("/home/derikon/cocaine/cocaine_config.json");
-		elconf.remotes.push_back(elliptics_proxy_t::remote(host, port, family));
-		elconf.success_copies_num = SUCCESS_COPIES_TYPE__ALL;
-
-		proxy.reset(new elliptics_proxy_t(elconf));
-		sleep(1);
-	}
-
-	void process(tests_t tests) {
-		size_t num = 0;
-
-		for (auto it = tests.begin(), end = tests.end(); it != end; ++it) {
-			std::cout << "Test #" << ++num << ": ";
-			try {
-				(*it)(*proxy);
-				std::cout << "Test #" << num << ": done" << std::endl;
-			} catch (...) {
-				std::cout << "Test #" << num << ": fail" << std::endl;
-			}
-		}
-	}
-
-private:
-	std::shared_ptr<elliptics_proxy_t> proxy;
 };
 
 #define ADD_TEST(name, func) suite.addTest(new elliptics_caller_t(name, &elliptics_tests_t:: func, elliptics_tests))
 #define ADD_TEST1(func) ADD_TEST(#func, func)
 
+void print_usage() {
+	std::cout << "Usage: appname [--remote hostname:port:family groups]" << std::endl;
+}
+
 int main(int argc, char* argv[])
 {
-	if (strcmp(argv[1], "--local")) {
-		CppUnit::TestResult controller;
-		std::unique_ptr<CppUnit::TestListener> listener(new JetBrains::TeamcityProgressListener());
-		controller.addListener(listener.get());
-		CppUnit::TestSuite suite;
-
-		elliptics_tests_t elliptics_tests(std::string(argv[1]) + "/manager.sh");
-		typedef CppUnit::TestCaller<elliptics_tests_t> elliptics_caller_t;
-
-		ADD_TEST("write and remove", remove);
-		ADD_TEST("simple write and read", write_read);
-		ADD_TEST("chunked write and read", write_chunk_read);
-		ADD_TEST("prepare-commit write and read", write_parts_read);
-		ADD_TEST("bulk write and bulk read", bulk_write_read);
-		ADD_TEST("async write and async read", async_write_read);
-		ADD_TEST("lookup", lookup);
-		ADD_TEST("write and read with embeds", write_read_with_embeds);
-
-		ADD_TEST("write into 4 groups; with SUCCESS_COPIES_TYPE__ALL", write_g4_scnALL);
-		ADD_TEST("write into 3 groups; with SUCCESS_COPIES_TYPE__ALL", write_g3_scnALL);
-		ADD_TEST("write into 3 groups, 1 group is shut down; with SUCCESS_COPIES_TYPE__ALL", write_g2_1_scnALL);
-		ADD_TEST("write into 3 groups; with SUCCESS_COPIES_TYPE__QUORUM", write_g3_scnQUORUM);
-		ADD_TEST("write into 3 groups, 1 group is shut down; with SUCCESS_COPIES_TYPE__QUORUM", write_g2_1_scnQUORUM);
-		ADD_TEST("write into 3 groups, 2 groups are shut down; with SUCCESS_COPIES_TYPE__QUORUM", write_g1_2_scnQUORUM);
-		ADD_TEST("write into 3 groups, 1 group is shut down; with SUCCESS_COPIES_TYPE__ANY", write_g2_1_scnANY);
-		ADD_TEST("write into 3 groups, 2 groups are shut down; with SUCCESS_COPIES_TYPE__ANY", write_g1_2_scnANY);
-		ADD_TEST("write into 3 groups, 3 groups are shut down; with SUCCESS_COPIES_TYPE__ANY", write_g0_3_scnANY);
-
-		suite.run(&controller);
-	} else {
-		std::string host = "localhost";
-		int port = 1025;
-		int family = 2;
-		switch (argc) {
-		case 5:
-			family = boost::lexical_cast<int>(argv[4]);
-		case 4:
-			port = boost::lexical_cast<int>(argv[3]);
-		case 3:
-			host.assign(argv[2]);
-			break;
-		default:
-			if (argc != 2) {
-				std::cout << "Usage:" << std::endl
-						  << std::cout << "name [host [port [family]]]" << std::endl;
-			}
+	std::shared_ptr<elliptics_manager_t> elliptics_manager;
+	if (strcmp(argv[1], "--remote") == 0) {
+		if (argc != 4) {
+			print_usage();
 			return 0;
 		}
 
-		tester t(host, port, family);
-		t.process({test_sync
-				  , test_sync_embeds
-				  , test_async
-				  , test_bulk_sync
-				  , test_lookup
-				  , test_mastermind_groups
-		});
+		typedef boost::char_separator<char> separator_t;
+	    typedef boost::tokenizer<separator_t> tokenizer_t;
+
+		std::string remote = argv[2];
+		separator_t sep(":");
+        tokenizer_t tok(remote, sep);
+
+		std::string hostname;
+		int port;
+		int family;
+
+		tokenizer_t::iterator tit = tok.begin();
+		hostname = *tit;
+		port = boost::lexical_cast<int>(*(++tit));
+		family = boost::lexical_cast<int>(*(++tit));
+
+		if (tit != tok.end()) {
+			print_usage();
+			return 0;
+		}
+	
+		std::string str_groups = argv[3];
+		std::vector<int> groups;
+		tokenizer_t tok2(str_groups, sep);
+		for (tokenizer_t::iterator it = tok2.begin(), end = tok2.end(); end != it; ++it) {
+			groups.push_back(boost::lexical_cast<int>(*it));
+		}
+
+		elliptics_manager.reset(new simple_manager_t(hostname, port, family, groups));		
+	} else {
+		elliptics_manager.reset(new script_manager_t(std::string(argv[1]) + "/manager.sh"));
 	}
+
+	CppUnit::TestResult controller;
+	std::unique_ptr<CppUnit::TestListener> listener(new JetBrains::TeamcityProgressListener());
+	controller.addListener(listener.get());
+	CppUnit::TestSuite suite;
+
+	elliptics_tests_t elliptics_tests(elliptics_manager);
+	typedef CppUnit::TestCaller<elliptics_tests_t> elliptics_caller_t;
+
+	ADD_TEST("write and remove", remove);
+	ADD_TEST("simple write and read", write_read);
+	ADD_TEST("chunked write and read", write_chunk_read);
+	ADD_TEST("prepare-commit write and read", write_parts_read);
+	ADD_TEST("bulk write and bulk read", bulk_write_read);
+	ADD_TEST("async write and async read", async_write_read);
+	ADD_TEST("lookup", lookup);
+	ADD_TEST("write and read with embeds", write_read_with_embeds);
+
+	ADD_TEST("write into 4 groups; with SUCCESS_COPIES_TYPE__ALL", write_g4_scnALL);
+	ADD_TEST("write into 3 groups; with SUCCESS_COPIES_TYPE__ALL", write_g3_scnALL);
+	ADD_TEST("write into 3 groups, 1 group is shut down; with SUCCESS_COPIES_TYPE__ALL", write_g2_1_scnALL);
+	ADD_TEST("write into 3 groups; with SUCCESS_COPIES_TYPE__QUORUM", write_g3_scnQUORUM);
+	ADD_TEST("write into 3 groups, 1 group is shut down; with SUCCESS_COPIES_TYPE__QUORUM", write_g2_1_scnQUORUM);
+	ADD_TEST("write into 3 groups, 2 groups are shut down; with SUCCESS_COPIES_TYPE__QUORUM", write_g1_2_scnQUORUM);
+	ADD_TEST("write into 3 groups, 1 group is shut down; with SUCCESS_COPIES_TYPE__ANY", write_g2_1_scnANY);
+	ADD_TEST("write into 3 groups, 2 groups are shut down; with SUCCESS_COPIES_TYPE__ANY", write_g1_2_scnANY);
+	ADD_TEST("write into 3 groups, 3 groups are shut down; with SUCCESS_COPIES_TYPE__ANY", write_g0_3_scnANY);
+
+	suite.run(&controller);
 	return 0;
 #if 0
 	//test_lookup ();
