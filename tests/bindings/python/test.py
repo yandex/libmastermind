0920988c80154fb644e2a6a890cf9ca13f3b30c5
derikon <derikon@yandex-team.ru>
2013-05-28 18:01:01 +0400

    Moved part of teamcity-messages to our test

--- tests/bindings/python/test.py
+++ tests/bindings/python/test.py
@@ -1,11 +1,10 @@
 # coding=utf-8
 import sys
 sys.path.append("bindings/python/")
-sys.path.append("/usr/local/lib/python2.7/dist-packages/teamcity_messages-1.7-py2.7.egg")
+#sys.path.append("/usr/local/lib/python2.7/dist-packages/teamcity_messages-1.7-py2.7.egg")
 print sys.path
 
-#from teamcity import is_running_under_teamcity
-from teamcity.unittestpy import TeamcityTestRunner
+from unittestpy import TeamcityTestRunner
 from elliptics_proxy import *
 
 import unittest
